348e72f4fae41f16fe84e5c75d9d08771ae8d5c7
Paulo Godoi <paulogodoi.sp@gmail.com>
2022-08-14 22:32:39 -0300

    teste

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,34 @@
-import { Login } from "../components/Login";
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { changeUser } from "../redux/userSlice"
 
-export default function LoginPage() {
-  return <Login />
+import styles from '../styles/login.module.scss'
+
+export default function Login() {
+  
+  const message = 'Insira o nome de login'
+  const [name, setName] = useState('')
+  const dispatch = useDispatch()
+
+  function handleLogin() {
+    if(name != '') {
+      dispatch(changeUser(name))
+      window.history.back()
+    } else {
+      alert(message)
+    }
+  }
+  
+  return (
+    <div className={styles.signContainer}>
+      <h1>Login</h1>
+      <input
+        className={styles.inputLogin} 
+        type="text" 
+        placeholder="Nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
 }
